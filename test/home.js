89bb797a97d65f23c37a74d8d2eaf2096ac48e72
Stefan Tenev <stefan.tenev@isobar.com>
2019-06-26 14:00:04 +0300

    Initial commit

---
+++ test/home.js
@@ -0,0 +1,16 @@
+describe("Home Page", () => {
+  beforeAll(async () => {
+    return page.goto("http://develop.headless.dev.ecm", {
+      waitUntil: 'networkidle0'
+    });
+  });
+
+  it("should have title containing Isobar", async () => {
+    expect(await page.title()).toContain("Isobar");
+  });
+
+  it('should contain at least 5 links', async () => {
+    const links = await page.$$('main a');
+    expect(links.length).toBeGreaterThanOrEqual(5);
+  })
+});
